51954cd723c9bf99a338ad7e69f5747db006e4e6
Rodrigo Oliveira <rodrigomengo06@gmail.com>
2022-05-27 18:00:36 -0300

    Remove duplicate school_name verification

--- src/api/components/school/controller.ts
+++ src/api/components/school/controller.ts
@@ -59,17 +59,7 @@ export class SchoolController {
 	createSchool = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
 		try {
 			const { name } = req.body;
-
-			const existingSchool: Array<School> = await this.repo.find({
-				where: {
-					name
-				}
-			});
-
-			if (existingSchool.length > 0) {
-				return res.status(400).json({ error: 'School name is already taken' });
-			}
-
+			
 			const school: School = this.repo.create({"name": name})
 
 			const newSchool = await this.repo.insert(school);
